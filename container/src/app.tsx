69613111c99c67d605583cc8a9e642bb5fc590e1
Avily Silva <avilyre@gmail.com>
2024-07-01 17:12:01 -0300

    feat: add aside with navbar

--- container/src/app.tsx
+++ container/src/app.tsx
@@ -7,13 +7,10 @@ const Navbar = lazy(() => import("NavbarApp/Navbar"));
 const App = () => {
   return (
     <body className="container">
-      <aside>
-        <Suspense fallback={<div>loading navbar...</div>}>
-          <Navbar />
-        </Suspense>
-      </aside>
+      <Suspense fallback={<div>loading navbar...</div>}>
+        <Navbar />
+      </Suspense>
       <main>
-
         <h1>Container</h1>
       </main>
     </body>
--- navbar/src/app.tsx
+++ navbar/src/app.tsx
@@ -1,8 +1,33 @@
+import "./styles/app.scss";
+
+import { HomeIcon, Star, Projector } from "lucide-react";
+import logo from "./assets/logo.svg"
+
 const App = () => {
   return (
-    <main>
-      <h1>Navbar</h1>
-    </main>
+    <aside className="aside-container">
+      <nav className="navbar-container">
+        <img className="logo-image" src={logo} alt="" />
+
+        <ul className="menu-list">
+          <li>
+            <a href="#">
+              <HomeIcon />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <Star />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <Projector />
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </aside>
   )
 }
 
