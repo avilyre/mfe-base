51f91649a3b2a4c057903ce1a7e97c03cd2a9c94
Avily Silva <avilyre@gmail.com>
2024-07-11 18:01:24 -0300

    chore: add custom fetch error class

--- container/src/services/get-movies.ts
+++ container/src/services/get-movies.ts
@@ -1,7 +1,12 @@
 import { ApiResponse } from "../@types/api-response";
 import { api } from "../lib/api";
+import { FetchError } from "../@data/errors/fetch-error";
 
 export const getMovies = async (url: string, pageNumber: number = 1) => {
-  const { data: movies } = await api.get<ApiResponse>(`${url}?api_key=${process.env.MOVIE_DB_API_KEY}&language=pt-BR&page=${pageNumber}`);
-  return movies.results;
+  try {
+    const { data: movies } = await api.get<ApiResponse>(`${url}?api_key=${process.env.MOVIE_DB_API_KEY}&language=pt-BR&page=${pageNumber}`);
+    return movies.results;
+  } catch {
+    throw new FetchError("Failed to fetch movies");
+  }
 }
